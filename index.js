d0753423fcb05ad01975639dfb2a7a43f9f60d7c
Hemil08 <hemilsorathiya008@gmail.com>
2026-03-15 20:05:18 +0100

    implement humanMessage functionality where the chat with ai can be stored

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import "dotenv/config"
-import readline from "readline"
+import readline from "readline/promises"
 import { ChatMistralAI } from "@langchain/mistralai"
+import { HumanMessage } from "langchain"
+
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -15,3 +17,21 @@ rl.question("What is your name? ",(name) => {
 const model = new ChatMistralAI({
     model: "mistral-small-latest",
 })
+
+const messages = []
+
+while (true){
+    const userInput = await rl.question("You: ")
+
+    messages.push[new HumanMessage(userInput)]
+    const response = await model.invoke(userInput)
+
+    messages.push(response)
+
+    console.log(response.text)
+
+}
+
+
+
+rl.close()
